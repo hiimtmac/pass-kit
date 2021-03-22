49de8f09e85015b7b0e8200795e0a65e04cfe4ff
hiimtmac <hiimtmac+github@gmail.com>
2021-03-22 08:09:05 -0500

    error

--- Sources/PassKit/PassError.swift
+++ Sources/PassKit/PassError.swift
@@ -5,3 +5,13 @@ enum PassError: Error {
     case passWrite
     case manifestWrite
 }
+
+extension PassError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .nonEmptyDirectory: return "target pass directory has contents in it"
+        case .passWrite: return "pass.json failed to write"
+        case .manifestWrite: return "manifest.json failed to write"
+        }
+    }
+}
