cb689946c362ea2e45f8e328fb11925772a6efd8
hiimtmac <hiimtmac+github@gmail.com>
2025-03-20 08:59:06 -0500

    bump certs

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // .package(url: "https://github.com/weichsel/ZIPFoundation.git", .upToNextMajor(from: "0.9.19")),
         .package(url: "https://github.com/weichsel/ZIPFoundation.git", branch: "development"),
-        .package(url: "https://github.com/apple/swift-certificates.git", from: "1.7.0")
+        .package(url: "https://github.com/apple/swift-certificates.git", from: "1.8.0")
     ],
     targets: [
         .target(
